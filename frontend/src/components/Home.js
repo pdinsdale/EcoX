0aa7e43480609ad4a6efad7cd3bfe150360f54ae
Parker Dinsdale <pdinsdale1128@gmail.com>
2025-02-15 15:31:20 -0500

    Homepage skeleton

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="Home">
+        <div className="app-name">
+
+        </div>
+        <div className="how-it-works">
+            <div className="header">
+
+            </div>
+            <div className="desc">
+
+            </div>
+        </div>
+        <div className="steps">
+            <div className="step1">
+                <img className="step1-img" />
+                <div className="step1-header">
+
+                </div>
+                <div className="step1-desc">
+
+                </div>
+            </div>
+            <img className="arrow-img" />
+            <div className="step2">
+                <img className="step2-img" />
+                <div className="step2-header">
+                    
+                </div>
+                <div className="step2-desc">
+                    
+                </div>
+            </div>
+            <img className="arrow-img" />
+            <div className="step3">
+                <img className="step3-img" />
+                <div className="step3-header">
+                    
+                </div>
+                <div className="step3-desc">
+                    
+                </div>
+            </div>
+        </div>
+        <div className="go-button">
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
